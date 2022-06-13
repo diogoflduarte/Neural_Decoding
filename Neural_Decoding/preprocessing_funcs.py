24fa56f34cf06922ad120f45904c3fb201ebb551
Diogo <diogo.fl.duarte@gmail.com>
2022-06-13 21:39:35 +0100

    flexible time selection of neural data

--- Neural_Decoding/preprocessing_funcs.py
+++ Neural_Decoding/preprocessing_funcs.py
@@ -117,3 +117,52 @@ def get_spikes_with_history(neural_data,bins_before,bins_after,bins_current=1):
         X[i+bins_before,:,:]=neural_data[start_idx:end_idx,:] #Put neural data from surrounding bins in X, starting at row "bins_before"
         start_idx=start_idx+1;
     return X
+
+def get_spikes_flexible_bins(neural_data, bin_array):
+    """
+    Function that creates the covariate matrix of neural activity.
+    Based on 'get_spikes_with_history', but with flexible bin selection: instead of a continuous bin selection, can
+    skip bins, use only bins in the past or future, etc. Useful for sanity checks and also to see if information about
+    behavior is reliably contained in past or future spiking
+
+    Parameters
+    ----------
+    neural_data: a matrix of size "number of time bins" x "number of neurons"
+        the number of spikes in each time bin for each neuron
+    bins_array: array of integers for the bins ot be used relative to the timepoint being predicted.
+        Current bin only:                                           [0]
+        One two bins to the past plus the current and one future:   [-2, -1, 0, 1] or np.arange(-2,1+1)
+        Only 5 bins in the past:                                    [-5]
+
+    Returns
+    -------
+    X: a matrix of size "number of total time bins" x "number of surrounding time bins used for prediction" x "number of neurons"
+        For every time bin, there are the firing rates of all neurons from the specified number of time bins before (and after)
+    """
+
+    num_examples = neural_data.shape[0] #Number of total time bins we have neural data for
+    num_neurons = neural_data.shape[1] #Number of neurons
+    bin_array = np.array(bin_array)
+    num_bins = bin_array.shape[0]
+    X=np.empty([num_examples,num_bins,num_neurons]) #Initialize covariate matrix with NaNs
+    X[:] = np.NaN
+
+    bins_before = np.abs(np.min((0, bin_array.min())))
+    bins_after  = np.abs(np.max((0, bin_array.max())))
+
+    bins_current = 0 in bins_array
+    bins_span = bins_before + bins_after + bins_current
+
+    #Loop through each time bin, and collect the spikes occurring in surrounding time bins
+    #Note that the first "bins_before" and last "bins_after" rows of X will remain filled with NaNs, since they don't get filled in below.
+    #This is because, for example, we cannot collect 10 time bins of spikes before time bin 8
+    start_idx=0
+    for i in range(num_examples-bins_before-bins_after): #The first bins_before and last bins_after bins don't get filled in
+
+        end_idx=start_idx+surrounding_bins; #The bins of neural data we will be including are between start_idx and end_idx (which will have length "surrounding_bins")
+        X[i+bins_before,:,:] = neural_data[start_idx:end_idx,:] #Put neural data from surrounding bins in X, starting at row "bins_before"
+        start_idx=start_idx+1;
+
+
+
+    return X
